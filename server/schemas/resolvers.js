e4d29695c80c920aa18d7b55033290d809806469
Hector <141383937+hfelix3@users.noreply.github.com>
2024-02-03 11:53:19 -0700

    edit to resolvers from user-controllers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,54 +1,98 @@
 const { Book, User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   // TODO:createUser, getSingleUser, saveBook, deleteBook, login
   Query: {
-    me: async () => {
-      // use create or save to save a book? need delete a book.
-      return Book.create({});
+    async createUser({ body }, res) {
+      const user = await User.create(body);
+  
+      if (!user) {
+        return res.status(400).json({ message: 'Something is wrong!' });
+      }
+      const token = signToken(user);
+      res.json({ token, user });
     },
-    user: async (parent {_id}) => {
-      const params = _id ? { _id} : {};
-      return User.
+
+    // TODO:getSingleUser
+    async getSingleUser({ user = null, params }, res) {
+      const foundUser = await User.findOne({
+        $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
+      });
+  
+      if (!foundUser) {
+        return res.status(400).json({ message: 'Cannot find a user with this id!' });
+      }
+  
+      res.json(foundUser);
     },
+
+    // TODO:saveBook,
+    // TODO:deleteBook,
+    // TODO:login
   },
   
-  Mutaion: {
+  Mutation: {
     // TODO:login: Accepts an email and password as parameters; returns an Auth type.
 
-    login: async (parent, {email, password}) => {
-      const params = {email, password};
-
-       // Call authentication service
-      const auth = await authService.login(params);
-
-        // Return Auth type
-      return {
-        token: auth.token,
-        user: {
-          id: auth.userId,
-          name: auth.name
-        },
-      },
+    async login({ body }, res) {
+      const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
+      if (!user) {
+        return res.status(400).json({ message: "Can't find this user" });
+      }
+  
+      const correctPw = await user.isCorrectPassword(body.password);
+  
+      if (!correctPw) {
+        return res.status(400).json({ message: 'Wrong password!' });
+      }
+      const token = signToken(user);
+      res.json({ token, user });
     },
+
 
     // TODO:addUser: Accepts a username, email, and password as parameters; returns an Auth type.
-    addUser: async () => {
-
+    async createUser({ body }, res) {
+      const user = await User.create(body);
+  
+      if (!user) {
+        return res.status(400).json({ message: 'Something is wrong!' });
+      }
+      const token = signToken(user);
+      res.json({ token, user });
     },
 
-// TODO:saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+    // TODO:saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
 
-    saveBook: async () => {
-
+    async saveBook({ user, body }, res) {
+      console.log(user);
+      try {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: user._id },
+          { $addToSet: { savedBooks: body } },
+          { new: true, runValidators: true }
+        );
+        return res.json(updatedUser);
+      } catch (err) {
+        console.log(err);
+        return res.status(400).json(err);
+      }
     },
 
-// TODO:removeBook: Accepts a book's bookId as a parameter; returns a User type.
-    removeBook: async () => {
-
+    // TODO:removeBook: Accepts a book's bookId as a parameter; returns a User type.
+    async deleteBook({ user, params }, res) {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: user._id },
+        { $pull: { savedBooks: { bookId: params.bookId } } },
+        { new: true }
+      );
+      if (!updatedUser) {
+        return res.status(404).json({ message: "Couldn't find user with this id!" });
+      }
+      return res.json(updatedUser);
     },
 
   },
-};
+}
 
 module.exports = resolvers;
