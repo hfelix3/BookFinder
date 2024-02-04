dffca560c366ddacd958b632a116f6ce3cb387bc
Hector <141383937+hfelix3@users.noreply.github.com>
2024-02-04 12:05:37 -0700

    updates

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,15 @@
 const typeDefs = `
-type User {
-    _id: ID
-    username: String
-    email: String
-    bookCount: Int
-    savedBooks: [Book]
+type Query {
+    users: [User]
+    user(username: String!): User
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String!, title: String!, bookId: ID, image: String!): User
+    removeBook(thoughtId: ID!, commentId: ID!): Thought
 }
 
 type Book {
